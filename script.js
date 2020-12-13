e11f11bb81003f9885675f62278e793a8e0f6074
Sarah Rainsberger <sarah@rainsberger.ca>
2020-12-13 16:35:10 +0000

    parsed the data for part 2

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ function separateLines(data) {
   return data.split("\n")
 }
 
+
 function removeExtra(data){
   let timestamp = data[0]
-  let validBusIDs = data[1].split(",").filter(id => id !== "x")
-  let busIDs = validBusIDs.map(id => parseInt(id))
-  return {timestamp, busIDs }
+  let validBusIDs = data[1].split(",")
+  let busIDs = validBusIDs.map(bus => [validBusIDs.indexOf(bus), parseInt(bus)])
+  let pairs = busIDs.filter(bus => (bus[0] !== 1))  
+  return {timestamp, pairs }
 }
+
+//HERE I HAVE A TIMESTAMP AND ARRAY OF MINUTES/BUS IDS
 
 function getSoonestBus(timestamp, busID){
   let soonestBus = (Math.ceil(timestamp / busID))*busID
@@ -37,5 +41,48 @@ function checkAllBus(data) {
 getAllData()
 .then(separateLines)
 .then(removeExtra)
-.then(results => checkAllBus(results))
+// .then(results => checkAllBus(results))
 .then(result => console.log(result)) 
+
+// let dataFile = 'day13.txt'
+
+// async function getAllData() {
+//   let response = await fetch(dataFile)
+//   let data = await response.text()
+//   return data
+// }
+
+// function separateLines(data) {
+//   return data.split("\n")
+// }
+
+
+// function removeExtra(data){
+//   let timestamp = data[0]
+//   let validBusIDs = data[1].split(",").filter(id => id !== "x")
+//   let busIDs = validBusIDs.map(id => parseInt(id))
+//   return {timestamp, busIDs }
+// }
+
+// function getSoonestBus(timestamp, busID){
+//   let soonestBus = (Math.ceil(timestamp / busID))*busID
+//   let waitTime = soonestBus - timestamp
+//   let productOfWaitBus = waitTime * busID
+//   return {waitTime, productOfWaitBus, busID }
+// }
+
+// function checkAllBus(data) {
+//   let nextBuses = []
+//   data.busIDs.map(bus => {
+//     nextBuses.push(getSoonestBus(data.timestamp,bus))
+//   })
+//   return nextBuses
+ 
+// }
+
+
+// getAllData()
+// .then(separateLines)
+// .then(removeExtra)
+// .then(results => checkAllBus(results))
+// .then(result => console.log(result)) 
